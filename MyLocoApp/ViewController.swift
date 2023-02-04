92fd9d43e74212c7040207b97fff7a8a83ff945f
Josue Gisber <jgisber@gmail.com>
2023-02-03 22:14:41 -0400

    Initial Commit

---
+++ MyLocoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLocoApp
+//
+//  Created by Josue Gisber on 2/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
